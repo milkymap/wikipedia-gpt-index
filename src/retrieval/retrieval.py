0085c31ce6c000d6dcab16172638012614787f8f
milkymap <ibrahima.elmokhtar@gmail.com>
2024-01-10 09:46:48 +0100

    define retrieval(llm)

---
+++ src/retrieval/retrieval.py
@@ -0,0 +1,58 @@
+
+import openai 
+
+from openai.types.chat import ChatCompletion, ChatCompletionChunk
+from src.schema.retrieval import Role, Message
+
+from pydantic import BaseModel
+
+from typing import List, Dict, Iterable 
+
+class LLModel:
+    def __init__(self, model_name:str, openai_api_key:str, description:str, forbidden_topics:List[str]):
+        self.forbidden_topics = forbidden_topics
+        self.description = description
+        self.model_name = model_name
+        self.client = openai.OpenAI(
+            api_key=openai_api_key
+        )
+    
+    def _build_system_setting(self, context:str) -> Message:
+        return Message(
+            role=Role.SYSTEM,
+            content=f"""
+                ROLE:
+                    you are an assistant that will help the user to explore this document : {self.description}
+                    your role is to help the user to apply retrieval augmented generation based on the following context:
+                    here is the context : ###
+                    {context}
+                    ###
+                USER/ASSISTANT FLOW:
+                             |--> greeting --> assistant --> reply greeting
+                             |--> ask information about your role --> assistant --> present yourself
+                             |--> ask information about the context --> try the build the response if there is one else tell the user that you was not able to build the response 
+                             |--> talk about forbidden topics --> assistant --> tell the user that your scope is limited to this document
+                IMPORTANT:
+                    FORBIDDEN-TOPICS={self.forbidden_topics}
+            """
+        )
+
+    def _build_messages(self, query:str, memory:List[Message]) -> List[Dict[str, str]]:
+        user_message = Message(
+            role=Role.USER,
+            content=query
+        )    
+        return [ mem.model_dump() for mem in memory ] + [ user_message.model_dump() ]
+
+    def analyse(self, query:str, context:str, memory:List[Message]) -> Iterable[ChatCompletionChunk]:
+        system_message = self._build_system_setting(context=context)
+        messages = [ system_message.model_dump() ] + self._build_messages(query, memory)
+        completion = self.client.chat.completions.create(
+            model=self.model_name,
+            messages=messages,
+            stream=True
+        )
+        return completion
+
+
+    
